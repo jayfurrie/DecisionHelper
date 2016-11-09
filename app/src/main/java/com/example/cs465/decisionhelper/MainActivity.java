8758700ac7f22efc56b0e0cb7f5381bb5c671488
David <dprorok2@illinois.edu>
2016-11-08 18:24:06 -0600

    Adds a tab bar to the main activity and 4 example tab layouts

--- app/src/main/java/com/example/cs465/decisionhelper/MainActivity.java
+++ app/src/main/java/com/example/cs465/decisionhelper/MainActivity.java
@@ -1,13 +1,70 @@
 package com.example.cs465.decisionhelper;
 
-import android.support.v7.app.AppCompatActivity;
+// code pulled from http://abhiandroid.com/ui/tabhost
+
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TabHost;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends TabActivity {
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost); // initiate TabHost
+        TabHost.TabSpec spec; // Reusable TabSpec for each tab
+        Intent intent; // Reusable Intent for each tab
+
+        spec = tabHost.newTabSpec("dh"); // Create a new TabSpec using tab host
+        spec.setIndicator("DH"); // set the “DH” as an indicator
+        // Create an Intent to launch an Activity for the tab (to be reused)
+        intent = new Intent(this, DecisionHelperActivity.class);
+        spec.setContent(intent);
+        tabHost.addTab(spec);
+
+        // Do the same for the other tabs
+
+        spec = tabHost.newTabSpec("pq"); // Create a new TabSpec using tab host
+        spec.setIndicator("PQ"); // set the “PQ” as an indicator
+        // Create an Intent to launch an Activity for the tab (to be reused)
+        intent = new Intent(this, PersonalityQuizActivity.class);
+        spec.setContent(intent);
+        tabHost.addTab(spec);
+
+        spec = tabHost.newTabSpec("settings"); // Create a new TabSpec using tab host
+        spec.setIndicator("S"); // set the “SETTINGS” as an indicator
+        // Create an Intent to launch an Activity for the tab (to be reused)
+        intent = new Intent(this, SettingsActivity.class);
+        spec.setContent(intent);
+        tabHost.addTab(spec);
+
+        spec = tabHost.newTabSpec("help"); // Create a new TabSpec using tab host
+        spec.setIndicator("H"); // set the “HELP” as an indicator
+        // Create an Intent to launch an Activity for the tab (to be reused)
+        intent = new Intent(this, HelpActivity.class);
+        spec.setContent(intent);
+        tabHost.addTab(spec);
+
+        //set tab which one you want to open first time 0 or 1 or 2
+        tabHost.setCurrentTab(0);
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                // display the name of the tab whenever a tab is changed
+                Toast.makeText(getApplicationContext(), tabId, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
+
+
 }
