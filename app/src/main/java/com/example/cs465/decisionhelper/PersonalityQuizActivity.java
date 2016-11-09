28047a6b879791e1bd9b655a8eaefa4323b90e33
David <dprorok2@illinois.edu>
2016-11-08 22:03:25 -0600

    Adds tabs

---
+++ app/src/main/java/com/example/cs465/decisionhelper/PersonalityQuizActivity.java
@@ -0,0 +1,13 @@
+package com.example.cs465.decisionhelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PersonalityQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab2);
+    }
+}
