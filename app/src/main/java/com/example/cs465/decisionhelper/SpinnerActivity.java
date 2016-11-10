15373b57aeea125bbe23c1e71d91528e9eda501b
snyoung2 <snyoung2@illinois.edu>
2016-11-10 16:11:50 -0600

    adds a new SpinnerActivity for the drop-down menus (spinners) and updates manifest

--- app/src/main/java/com/example/cs465/decisionhelper/SpinnerActivity.java
+++ app/src/main/java/com/example/cs465/decisionhelper/SpinnerActivity.java
@@ -1,7 +1,11 @@
 package com.example.cs465.decisionhelper;
 
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,6 +17,7 @@ public class SpinnerActivity extends DecisionHelperActivity implements AdapterVi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.dh_factors_dropdown);
 
         Spinner spinner = (Spinner) findViewById(R.id.possibility_spinner);
         // Spinner click listener
@@ -24,6 +29,17 @@ public class SpinnerActivity extends DecisionHelperActivity implements AdapterVi
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_SHOW_TITLE);
     }
 
     @Override
@@ -36,5 +52,16 @@ public class SpinnerActivity extends DecisionHelperActivity implements AdapterVi
     }
     public void onNothingSelected(AdapterView<?> arg0) {
         // TODO Auto-generated method stub
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
