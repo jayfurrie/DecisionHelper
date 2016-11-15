65442cc6ebc1e4672c4566762d7ad8d896716209
snyoung2 <snyoung2@illinois.edu>
2016-11-15 14:59:11 -0600

    links the dh UI screens together adds a pop-up dialog box for entering the factors and choices

--- app/src/main/java/com/example/cs465/decisionhelper/dh_add_choices.java
+++ app/src/main/java/com/example/cs465/decisionhelper/dh_add_choices.java
@@ -1,17 +1,70 @@
 package com.example.cs465.decisionhelper;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * Created by HiiLab on 11/13/16.
- */
 
 public class dh_add_choices extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    final Context context = this;
+    private Button button;
+    private EditText result;
+
+    public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dh_add_choices);
 
+        // find the add new choice button
+        button = (Button) findViewById(R.id.dh_add_choices_btn_newchoice);
+        // add button listener
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // get prompts.xml view
+                LayoutInflater li = LayoutInflater.from(context);
+                View promptsView = li.inflate(R.layout.activity_dh_addchoice_dialog, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+
+                // set prompts.xml to alertdialog builder
+                alertDialogBuilder.setView(promptsView);
+
+                final EditText userInput = (EditText) promptsView
+                        .findViewById(R.id.dh_addchoice_dialog_et);
+
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        // ToDo get user input here and save
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cs465/decisionhelper/dh_homepage.java
+++ app/src/main/java/com/example/cs465/decisionhelper/dh_homepage.java
@@ -1,7 +1,9 @@
 package com.example.cs465.decisionhelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class dh_homepage extends AppCompatActivity {
 
@@ -9,8 +11,10 @@ public class dh_homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dh_homepage);
-
-
     }
-
+    public void dh_homepage_btn_newdecisionOnClick(View view)
+    {
+        Intent intent = new Intent(this, dh_decision_menu.class);
+        startActivity(intent);
+    }
 }
