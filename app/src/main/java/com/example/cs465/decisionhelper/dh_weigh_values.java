d1c497ca1df8b105366a82d7c0555f389bdc810a
Jeff Hong <hong66@illinois.edu>
2016-11-15 19:54:11 -0600

    refactors possibilities to values links many screens together

--- app/src/main/java/com/example/cs465/decisionhelper/dh_weigh_values.java
+++ app/src/main/java/com/example/cs465/decisionhelper/dh_weigh_values.java
@@ -5,17 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class dh_rank_factors extends AppCompatActivity {
+public class dh_weigh_values extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dh_rank_factors);
+        setContentView(R.layout.activity_dh_weigh_values);
     }
 
-    public void dh_rank_factors_btn_backOnClick(View view)
+    public void dh_rank_values_btn_backOnClick(View view)
     {
-        Intent intent = new Intent(this, dh_decision_menu.class);
+        Intent intent = new Intent(this, dh_add_values.class);
         startActivity(intent);
     }
 }
