cd43f7a5c5daa8300cbb8f68bca9fcd9fc75884f
Jeff Hong <hong66@illinois.edu>
2016-11-16 19:45:19 -0600

    adds return button to pq_results

--- app/src/main/java/com/example/cs465/decisionhelper/pq_results.java
+++ app/src/main/java/com/example/cs465/decisionhelper/pq_results.java
@@ -1,7 +1,9 @@
 package com.example.cs465.decisionhelper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class pq_results extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class pq_results extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pq_results);
+    }
+
+    public void pq_results_btn_backOnClick(View view)
+    {
+        Intent intent = new Intent(this, dh_homepage.class);
+        startActivity(intent);
     }
 }
