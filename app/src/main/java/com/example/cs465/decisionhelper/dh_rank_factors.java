3d1923ceccd5e205edfaeb5145b68c15b8d170e5
wmtdhe <yaoyhand@sina.cn>
2016-11-30 02:09:17 -0600

    Rank_value

--- app/src/main/java/com/example/cs465/decisionhelper/dh_rank_factors.java
+++ app/src/main/java/com/example/cs465/decisionhelper/dh_rank_factors.java
@@ -1,17 +1,45 @@
 package com.example.cs465.decisionhelper;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class dh_rank_factors extends BaseActivity {
-
+    int decisionID;
+    int max=100;
+    int factorId;
+    final Context context = this;
+   // SeekBar sb[]=new SeekBar[100];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dh_rank_factors);
         setTitle("Rank Factors");
+        decisionID = getIntent().getIntExtra("decision_id", 0);
+        LinearLayout myFactors = (LinearLayout) findViewById(R.id.rank_factor);
+      // myFactors.removeAllViews();
+        List<Storage.Factor> factors = db.getAllFactorsForDecision(decisionID);
+        for (int i = 0; i < factors.size(); i++) {
+            Storage.Factor f = factors.get(i);
+            SeekBar sb=new SeekBar(context);
+            //sb=new SeekBar(context);
+            //sb.setId(i);
+           // sb[i].setMax( max );
+            TextView textView = new TextView(this);
+            textView.setText("How do you care about "+f.name+"?");
+            textView.setTextSize(20);
+            textView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+           // if(!myFactors.equals(null))
+            myFactors.addView(textView);
+            myFactors.addView(sb);
+        }
     }
 
     public void dh_rank_factors_btn_cancelOnClick(View view)
@@ -26,4 +54,5 @@ public class dh_rank_factors extends BaseActivity {
         startActivity(intent);
         // TODO save the factor rankings from user input on the slider bars
     }
+
 }
