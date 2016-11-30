64aee66cf7fb36af4a20bdbbb3f522b854119d51
Jeff Hong <hong66@illinois.edu>
2016-11-30 16:01:49 -0600

    rank_values_db
    stores seekbar data into db for values ranking

--- app/src/main/java/com/example/cs465/decisionhelper/dh_rank_values.java
+++ app/src/main/java/com/example/cs465/decisionhelper/dh_rank_values.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -14,11 +15,14 @@ import android.widget.TextView;
 
 import java.util.List;
 
+import static java.lang.Integer.parseInt;
+
 public class dh_rank_values extends BaseActivity {
     //int decisionID;
     int factorID;
     String factorName;
     int sbMax = 100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,11 +56,13 @@ public class dh_rank_values extends BaseActivity {
             textView.setGravity(Gravity.BOTTOM);
             valuesLayout.addView(textView);
 
+
             // add new seek bar
             SeekBar seekBar = new SeekBar(this);
             seekBar.setMax(sbMax);
+            seekBar.setTag(Integer.toString(v.id));
             valuesLayout.addView(seekBar);
-            // trying to add the text view scales..... didn't work. App crashed.
+            // trying to add the text view scales..... didn't work. App crashed. (not a priority for now).
             //LinearLayout scaleLayout = (LinearLayout) findViewById(R.id.dh_rank_values_tv_sb_scale);
             //valuesLayout.addView(scaleLayout);
             //LinearLayout scaleLayout = new LinearLayout(this);
@@ -66,6 +72,7 @@ public class dh_rank_values extends BaseActivity {
             //scaleLayout.addView(tvMin);
             //scaleLayout.addView(tvMax);
             // TODO add the the text view scales for the seek bar (0 to 100)
+
         }
 
     }
@@ -76,7 +83,34 @@ public class dh_rank_values extends BaseActivity {
 
     public void dh_rank_values_btn_OKOnClick(View view)
     {
+        //this stores the score
+        int sbScore;
+        //this stores the valueID
+        int sbID;
         Intent intent = new Intent(this, dh_list_rankvalues.class);
         startActivity(intent);
+        LinearLayout valuesLayout = (LinearLayout) findViewById(R.id.dh_rank_values_ll_list);
+        //grabs all the values related to this factor
+        List<Storage.Value> values = db.getAllValuesForFactor(factorID);
+
+        for (int i = 0; i < valuesLayout.getChildCount(); i++) {
+            // need to get the child at (i)
+            View child = (View) valuesLayout.getChildAt(i);
+            //valueID = (int) db.createValueForFactor(textView.getText().toString(), factorID);
+            // check if view is a seek bar
+            if (child.getClass().getName().equalsIgnoreCase("android.widget.SeekBar")){
+                // if its a seek bar, get the value and save it to the db for the previous value ID
+                SeekBar sb = (SeekBar) valuesLayout.getChildAt(i);
+                // get the seek bar value
+                sbScore = sb.getProgress();
+                sbID = Integer.parseInt(sb.getTag().toString());
+                // save to the db
+                Log.d("debug", "[storing] id: " + Integer.toString(sbID) + " value: " + Integer.toString(sbScore));
+                db.assignScoreToValue(login_login.username,sbScore,sbID);
+            }
+
+            //if not a seekbar, ignore
+
+        }
     }
 }
