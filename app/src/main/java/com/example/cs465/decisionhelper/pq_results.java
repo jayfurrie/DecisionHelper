bf11f79f0babe4fbca34db095e4a478225216893
Jeff Hong <hong66@illinois.edu>
2016-11-30 20:32:14 -0600

    pq_results db calls
    Grabs top choice and score from DB and displays inside two new textviews

--- app/src/main/java/com/example/cs465/decisionhelper/pq_results.java
+++ app/src/main/java/com/example/cs465/decisionhelper/pq_results.java
@@ -3,7 +3,9 @@ package com.example.cs465.decisionhelper;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class pq_results extends BaseActivity {
 
@@ -12,6 +14,20 @@ public class pq_results extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pq_results);
         setTitle("Result:");
+
+
+        int decisionID = getIntent().getIntExtra("decision_id", currDecisionID);
+        Log.d("debug", "grabbing top choice for decisionID: " + Integer.toString(decisionID));
+        Storage.Choice topChoice = db.getTopChoiceForDecision(decisionID);
+
+        String choiceName = topChoice.name;
+        String choiceScore = Double.toString(topChoice.score);
+
+        TextView tv_choiceName = (TextView) findViewById(R.id.pq_results_tv_result_choice);
+        TextView tv_choiceScore = (TextView) findViewById(R.id.pq_results_tv_result_score);
+
+        tv_choiceName.setText("Your top choice is: "+ choiceName);
+        tv_choiceScore.setText("It has a score of: " + choiceScore);
     }
 
     public void pq_results_btn_backOnClick(View view)
